e62439bf72d67e866869b8211556ad2bd582c3c3
greatjing <greatjing@sina.cn>
2017-06-23 15:53:41 +0800

    implement product index and show

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
     resources :products
   end
 
+  resources :products
   #root 'admin/products#index
-  root 'welcome#index'
+  #root 'welcome#index'
+  root 'products#index'
 end
