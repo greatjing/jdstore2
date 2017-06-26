e5cd116c52b7f1951e1e6b59e2700071e5c81935
greatjing <greatjing@sina.cn>
2017-06-26 14:25:13 +0800

    add order controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  has_many :orders
+  
 #是否是admin
   def admin?
     is_admin
